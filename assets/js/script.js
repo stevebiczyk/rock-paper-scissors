7ab9fff92dc1a915a126e038eaec9c249b9b2e86
stevebiczyk <stevebiczyk@gmail.com>
2022-11-02 22:38:36 +0000

    Completed Playerselection and Coumputerchoice functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,40 @@
  * Declare constants for DOM elements and possible choices.
  */
 
-const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
-const choices = ["rock", "paper", "scissors"]
-
-/**
- * Add event listeners to the buttons.
- */
-function playerChoice() {
-
-}
-
-function computerChoice() {
-
-}
-
-function playGame() {
-
-}
-
-function decideWinner () {
-
-}
-
-function incrementPlayerScore() {
-
-}
-function incrementComputerScore() {
+ const playerScore = document.getElementById("player-score");
+ const computerScore = document.getElementById("computer-score");
+ const choices = ["rock", "paper", "scissors"]
+ 
+ /**
+  * Add event listeners and get player selection.
+  */
+  let selections = document.getElementsByClassName("selection");
+     for (let selection of selections) {
+         selection.addEventListener("click", function() {
+ let playerSelection = this.getAttribute("data-selection");
+ playGame(playerSelection);
+         });
+     }
     
-}
+ function generateComputerChoice() {
+ /* const choices = ["rock", "paper", "scissors"] */
+    let randomSelection = Math.floor(Math.random() * choices.length);
+    return choices[randomSelection];
+ }
+ 
+ function playGame(playerSelection) {
+    const computerChoice = generateComputerChoice();
+    console.log(`Player chose ${playerSelection}`);
+    console.log(`Computer chose ${computerChoice}`);
+ }
+ 
+ function decideWinner () {
+ 
+ }
+ 
+ function incrementPlayerScore() {
+ 
+ }
+ function incrementComputerScore() {
+     
+ }
