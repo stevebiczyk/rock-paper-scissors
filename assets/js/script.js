d46e362115f10230c019f9308f3de6b758474261
stevebiczyk <stevebiczyk@gmail.com>
2022-11-02 19:00:47 +0000

    Added game area with clickable images for player selection, player prompt and result message.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Declare constants for DOM elements and possible choices.
+ */
+
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const choices = ["rock", "paper", "scissors"]
+
+/**
+ * Add event listeners to the buttons.
+ */
+function incrementPlayerScore() {
+
+}
+function incrementComputerScore() {
+    
+}
